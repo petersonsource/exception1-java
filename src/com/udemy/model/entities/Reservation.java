66a22d2072622379abbe2b570aeec8ea0b6848ea
Peterson dos Santos <peterson.opensource@hotmail.com>
2023-11-03 21:22:21 -0300

    bad solution

--- src/com/udemy/model/entities/Reservation.java
+++ src/com/udemy/model/entities/Reservation.java
@@ -39,9 +39,19 @@ public class Reservation {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
 	}
 	
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		
+		Date now = new Date();
+		if(checkIn.before(checkOut) || checkOut.before(now)) {
+			return "Reservation dates for update must be future dates";
+		}
+		if(!checkOut.after(checkIn)) {
+			return "Checkout data must be after check-in date";
+		}
+					
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 	
 	@Override
